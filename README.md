296bfe0c501765e503d7f41fe8aa856c46406134
Jason <jasonct454@gmail.com>
2025-06-19 02:42:00 +0200

    welcome button fix

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
 # u1-bw1-d1
 
----AGGIORNAMENTO DEL 18/06/2025 delle 04:00
+----AGGIORNAMENTO 19/06/2025
 
--fix & change ai file:
-
-- |2exam.html
-  |2exam.css
-  |2exam.js add timer
-
-- result.js
-  placeholder grafico con score
-
----
-
--piccoli fix
-
-- welcome.css
-  background
-  grandezza
-- style_fb.css
-  stelline contorno
-  input text nero
-  bottone-sfocato
+- welcome files
+  button sistemato.
--- assets/script/welcome.js
+++ assets/script/welcome.js
@@ -1,11 +1,42 @@
+// const checkbox = document.getElementById("check");
+// const bottone = document.getElementById("procedi");
+
+// bottone.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   if (checkbox.checked) {
+//     window.location.href = "2exam.html";
+//   } else {
+//     alert("Spunta la casella!");
+//   }
+// });
+
+// const checkbox = document.getElementById("check");
+// const bottone = document.getElementById("procedi");
+
+// // bottone.classList.add("bottoneAttivo");
+
+// bottone.addEventListener("click", function (e) {
+//   e.preventDefault();
+//   if (checkbox.checked) {
+//     window.location.href = "2exam.html";
+//   }
+// });
 const checkbox = document.getElementById("check");
 const bottone = document.getElementById("procedi");
+
+checkbox.addEventListener("change", function () {
+  if (checkbox.checked) {
+    bottone.disabled = false;
+    bottone.classList.add("bottoneAttivo");
+  } else {
+    bottone.disabled = true;
+    bottone.classList.remove("bottoneAttivo");
+  }
+});
 
 bottone.addEventListener("click", function (e) {
   e.preventDefault();
   if (checkbox.checked) {
     window.location.href = "2exam.html";
-  } else {
-    alert("Spunta la casella!");
   }
 });
